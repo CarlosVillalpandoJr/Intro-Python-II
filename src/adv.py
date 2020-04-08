b12c92250e6b6418dff41143dd10206baecf5232
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-04-08 15:43:45 -0500

    constructed Player and Room classes

--- src/adv.py
+++ src/adv.py
@@ -4,7 +4,7 @@ from room import Room
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                    "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -49,3 +49,4 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+print(room)
